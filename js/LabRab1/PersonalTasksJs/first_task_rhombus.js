399d3762173a51ccd9bfba5a7b20c9426f1f69a0
Roman <sektor_wins@mail.ru>
2017-10-13 13:11:05 +0500

    Изменен код в скрипте к 1 доп. заданию 1ЛР: Вместо треугольника отображается ромб с диагональю

---
+++ js/LabRab1/PersonalTasksJs/first_task_rhombus.js
@@ -0,0 +1,109 @@
+var gl;
+var shaderProgram;
+var vertexBuffer;
+// установка шейдеров
+function initShaders() {
+    // получаем шейдеры
+    var fragmentShader = getShader(gl.FRAGMENT_SHADER, 'shader-fs');
+    var vertexShader = getShader(gl.VERTEX_SHADER, 'shader-vs');
+    //создаем объект программы шейдеров
+    shaderProgram = gl.createProgram();
+    // прикрепляем к ней шейдеры
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    // связываем программу с контекстом webgl
+    gl.linkProgram(shaderProgram);
+      
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert("Не удалсь установить шейдеры");
+    }
+      
+    gl.useProgram(shaderProgram);
+    // установка атрибута программы
+    shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+    // делаем доступным атрибут для использования
+    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+}
+// Функция создания шейдера по типу и id источника в структуре DOM
+function getShader(type,id) {
+    var source = document.getElementById(id).innerHTML;
+    // создаем шейдер по типу
+    var shader = gl.createShader(type);
+    // установка источника шейдера
+    gl.shaderSource(shader, source);
+    // компилируем шейдер
+    gl.compileShader(shader);
+   
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert("Ошибка компиляции шейдера: " + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);   
+        return null;
+    }
+    return shader;  
+}
+// установка буфера вершин 
+function initBuffers() {
+ // установка буфера вершин
+  vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  // массив координат вершин объекта
+  var rhombusVertices = [
+         -0.8,  0.0,  0.0,
+        0.0, 0.8,  0.0,
+         0.8, 0.0,  0.0,
+         0.0, -0.8,  0.0
+  ];
+  indices = [0, 1, 2, 3, 0,2];
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(rhombusVertices), gl.STATIC_DRAW);
+  // указываем кол-во точек
+  vertexBuffer.itemSize = 3;
+  //vertexBuffer.numberOfItems = 4;
+  indexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+  // указываем число линий. это число равно числу индексов
+  indexBuffer.numberOfItems = indices.length;
+}
+// отрисовка 
+function draw() {    
+    // установка области отрисовки
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+ 
+    gl.clear(gl.COLOR_BUFFER_BIT);
+   
+    // указываем, что каждая вершина имеет по три координаты (x, y, z)
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 
+                         vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    // отрисовка примитивов - треугольников          
+    gl.drawElements(gl.LINE_LOOP, indexBuffer.numberOfItems, gl.UNSIGNED_SHORT,0);
+}
+  
+window.onload=function(){
+    // получаем элемент canvas
+    var canvas = document.getElementById("canvas3D");
+    try {
+        // Сначала пытаемся получить стандартный контекст WegGL
+        // Если не получится, обращаемся к экспериментальному контексту
+        gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    }
+    catch(e) {}
+   
+    // Если контекст не удалось получить, выводим сообщение
+      if (!gl) {
+        alert("Ваш браузер не поддерживает WebGL");
+      }
+    if(gl){
+        // установка размеров области рисования
+        gl.viewportWidth = canvas.width;
+        gl.viewportHeight = canvas.height;
+        // установка шейдеров 
+        initShaders();
+        // установка буфера вершин
+        initBuffers();
+        // покрасим фон в бледно-розовый цвет
+        gl.clearColor(1.0, 0.0, 0.0, 0.5);
+        // отрисовка сцены
+        draw();  
+    }
+}
