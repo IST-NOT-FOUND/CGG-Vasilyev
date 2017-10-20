9a438433f35f778c5851c8abfa2bd65546bee2fe
Roman <sektor_wins@mail.ru>
2017-10-21 01:21:38 +0500

    Реализована смена цветов ромба

---
+++ js/LabRab1/PersonalTasksJs/test2.js
@@ -0,0 +1,137 @@
+var gl;
+var shaderProgram;
+var vertexBuffer;
+var colorBuffer;
+
+
+
+function initShaders() {
+    
+    var fragmentShader = getShader(gl.FRAGMENT_SHADER, 'shader-fs');
+    var vertexShader = getShader(gl.VERTEX_SHADER, 'shader-vs');
+    
+    shaderProgram = gl.createProgram();
+
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+
+    gl.linkProgram(shaderProgram);
+      
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert("Не удалсь установить шейдеры");
+    }
+      
+    gl.useProgram(shaderProgram);
+    shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+    shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
+    gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
+}
+
+function getShader(type,id) {
+    var source = document.getElementById(id).innerHTML;
+
+    var shader = gl.createShader(type);
+  
+    gl.shaderSource(shader, source);
+  
+    gl.compileShader(shader);
+   
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert("Ошибка компиляции шейдера: " + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);   
+        return null;
+    }
+    return shader;  
+}
+
+function initBuffers() {
+
+    var vertices = [
+        0.0, 0.5, 0.0,
+        0.5, 1.0, 0.0,
+        1.0, 0.5, 0.0,
+        0.5, 0.0, 0.0,
+        0.0 ,0.5, 0.0,
+        1.0, 0.5, 0.0,
+    ]
+
+    vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    vertexBuffer.itemSize = 3;
+    vertexBuffer.numberOfItems = 6;
+
+
+    x = getRandomValue(15, 200);
+    y = getRandomValue(15, 200);
+    z = getRandomValue(15, 200);
+    q = getRandomValue(15, 200);
+    w = getRandomValue(15, 200);
+    i = getRandomValue(15, 200);
+    var сolors = [
+          x, y, z,
+          x, y, z,
+          x, y, z,
+          q, w, i,
+          q, w, i,
+          q, w, i
+      ];
+    // var сolors = [
+    //     1.0, 0.0, 0.0, 
+    //     1.0, 0.0, 0.0, 
+    //     1.0, 0.0, 0.0, 
+    //     0.0, 1.0, 1.0, 
+    //     0.0, 1.0, 1.0, 
+    //     0.0, 1.0, 1.0
+    // ];
+
+      colorBuffer = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(сolors), gl.STATIC_DRAW);
+}
+
+function draw() {
+    
+        gl.clearColor(0.0, 1.0, 0.5, 1.0);
+        gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+    
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,
+                             vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    
+        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+        gl.vertexAttribPointer(shaderProgram.vertexColorAttribute,
+                            vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertexBuffer.numberOfItems);
+    }
+        
+window.onload=function(){
+        
+            var canvas = document.getElementById("canvas3D");
+            try {
+                gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+            }
+            catch(e) {}
+        
+              if (!gl) {
+                alert("Ваш браузер не поддерживает WebGL");
+              }
+            if(gl){
+                gl.viewportWidth = canvas.width;
+                gl.viewportHeight = canvas.height;
+        
+                initShaders();
+        
+                initBuffers();
+        
+                draw();
+            }
+}
+
+function getRandomValue(min, max) {
+    return (Math.random() * (max - min) + min) / 100;
+}
+
